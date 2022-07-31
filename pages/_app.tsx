75e6747f7ebcafba7ae5883f26af91c4b339962f
Pedro <pedrohjleite@hotmail.com>
2022-07-31 12:10:36 -0300

    First Commit 🎉

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,42 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
+
+declare global {
+  interface Window {
+    // eslint-disable-next-line
+    adstart: any;
+  }
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Script strategy='afterInteractive' id="pixel-sdk" dangerouslySetInnerHTML={{
+      __html: `
+        (function () {
+          var script = document.createElement('script');
+  
+          script.type = 'text/javascript';
+          script.async = true;
+          script.src = 'https://d39k0bjfwb8suf.cloudfront.net/sdk.js';
+          script.crossOrigin = 'anonymous'
+  
+          var x = document.getElementsByTagName('script')[0];
+          x.parentNode.insertBefore(script, x);
+        })();
+  
+        window.adstartAsyncInit = function () {
+          adstart.init({
+            pixelId: 'PIXEL_ID2s',
+            isInDebugMode: true,
+          })
+        }
+      `
+    }} />
+    <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
---
+++ pages/product.tsx
@@ -0,0 +1,5 @@
+export default function Producg() {
+  return (
+    <h1>Producg</h1>
+  )
+}
